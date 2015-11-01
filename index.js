e41b842c8111fef94d0b0e2b0dc7ba2b02335c25
Tony Spiro <tspiro@tonyspiro.com>
2015-11-01 13:20:36 -0600

    Universal :rocket:

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('es6-promise').polyfill()
-require('isomorphic-fetch')
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 var _ = require('lodash');
 
 var api_url = 'https://api.cosmicjs.com';
