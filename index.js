231b96413225e2f7058e20ff2500bdd427b049ec
Jazib Sawar <jazibsawar@gmail.com>
2017-09-24 03:03:36 +0500

    Added deleteMedia method

--- index.js
+++ index.js
@@ -273,6 +273,29 @@ module.exports = {
         }
         return callback(false, response);
     });
+  },
+
+  deleteMedia: function(config, object, callback){
+    var endpoint = api_url + '/' + api_version + '/' + config.bucket.slug + '/media/' + object.media_id;
+    fetch(endpoint, {
+      method: 'delete',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(object)
+    })
+    .then(function(response){
+      if (response.status >= 400) {
+        var err = {
+          'message': 'There was an error with this request.'
+        }
+        return callback(err, false);
+      }
+      return response.json()
+    })
+    .then(function(response){
+      return callback(false, response);
+    });
   }
 };
 
