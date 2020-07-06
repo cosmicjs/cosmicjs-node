a91cc72c28be424db1cbe9df6ac67a17f0892879
jazibsawar <jazibsawar@gmail.com>
2020-07-06 18:27:57 +0300

    Added: latest build

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
 var _require = require('./src/helpers/request_handler'),
     init = _require.init;
 
 var mainMethods = require('./src/main');
+
 var bucketMethods = require('./src/bucket');
 
 var Cosmic = function Cosmic(config) {
-  init(config);
-  // Combine methods
+  init(config); // Combine methods
+
   var methods = {
     bucket: bucketMethods
   };
