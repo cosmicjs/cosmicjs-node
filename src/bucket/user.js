7e08d505af19bead7a4ebe9dcfa20a3861ad4193
Tony Spiro <tspiro@tonyspiro.com>
2022-03-23 15:23:38 -0700

    fix user methods

--- src/bucket/user.js
+++ src/bucket/user.js
@@ -5,30 +5,15 @@ let headers;
 
 const userMethods = (bucket_config) => ({
   getUsers: () => {
-    const endpoint = `${URI}/${bucket_config.slug}/users`
-    if (bucket_config.write_key) {
-      headers = {
-        "Authorization": `Bearer ${bucket_config.write_key}`
-      }
-    }
+    const endpoint = `${URI}/buckets/${bucket_config.slug}/users`
     return requestHandler(HTTP_METHODS.GET, endpoint, null, headers)
   },
   getUser: (params) => {
-    const endpoint = `${URI}/${bucket_config.slug}/users/${params.id}`
-    if (bucket_config.write_key) {
-      headers = {
-        "Authorization": `Bearer ${bucket_config.write_key}`
-      }
-    }
+    const endpoint = `${URI}/buckets/${bucket_config.slug}/users/${params.id}`
     return requestHandler(HTTP_METHODS.GET, endpoint, null, headers)
   },
   addUser: (params) => {
-    const endpoint = `${URI}/${bucket_config.slug}/users`
-    if (bucket_config.write_key) {
-      headers = {
-        "Authorization": `Bearer ${bucket_config.write_key}`
-      }
-    }
+    const endpoint = `${URI}/buckets/${bucket_config.slug}/users`
     return requestHandler(HTTP_METHODS.POST, endpoint, params, headers)
   }
 })
